c7e6f7cdbc045dff3ab1e15060a98980e23bac1c
Maksim Rudenko <rudenko.programmer@gmail.com>
2019-02-04 00:12:51 +0200

    Episode 16

--- app/Project.php
+++ app/Project.php
@@ -15,4 +15,8 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
 	protected $guarded = [];
+
+	public function tasks(){
+		return $this->hasMany(Task::class);
+	}
 }
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,5 +4,14 @@
 
 @section('content')
     <h1 class="title">Project "{{ $project->title }}"</h1>
-    <p>{{ $project->description }}</p>
+    <div class="content">{{ $project->description }}</div>
+    <div>
+        @if($project->tasks->count())
+        <ul>
+            @foreach($project->tasks as $task)
+                <li>{{ $task->description }}</li>
+            @endforeach
+        </ul>
+        @endif
+    </div>
 @endsection
